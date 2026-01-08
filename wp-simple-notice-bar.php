61a0f7610ebdf5b4bef42d07f50022978922ad27
Ahmed Amir Hossain <108435781+amirdev99@users.noreply.github.com>
2026-01-08 12:55:37 +0600

    Update wp-simple-notice-bar.php

--- wp-simple-notice-bar.php
+++ wp-simple-notice-bar.php
@@ -7,7 +7,7 @@
  * Requires at least: 5.2
  * Requires PHP:      7.4
  * Author:            Amir
- * Author URI:        https://amir.example.com/
+ * Author URI:        https://amirkhan.example.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:      wsnb
